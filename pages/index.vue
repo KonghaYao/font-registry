cedd7165360971201c2fff954f8745957c2c14ee
江夏尧 <3446798488@qq.com>
2025-01-08 20:38:04 +0800

    feat: 初始化 nuxt 项目

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+// client.from('packages')
+</script>
+
+<template>
+ <div>{{ user?.email }}</div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+const { auth } = useSupabaseClient()
+watchEffect(() => {
+  if (user.value) {
+    navigateTo('/')
+  }
+})
+const login = ()=>{
+ auth.signInWithPassword({
+   email: 'fonts-1@snapmail.cc',
+   password: '123456789aA',
+ })
+}
+</script>
+
+<template>
+  <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <h2 class="my-6 text-center text-3xl font-extrabold">
+      Sign in to your account
+    </h2>
+    <button @click="login">login</button>
+  </div>
+</template>
