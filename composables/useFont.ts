7c0d92978346a1346422c655a1fc718e3d7af701
江夏尧 <3446798488@qq.com>
2025-01-25 19:09:34 +0800

    feat: 更改 CDN 位置

--- composables/useFont.ts
+++ composables/useFont.ts
@@ -1,4 +1,4 @@
 export const createFontLink = (pkgName: string, versionName: string, fileName: string) => {
     const file_folder = ("/packages/" + pkgName + "/" + versionName + "/" + fileName).replaceAll(".", "_");
-    return `/api/font-source/${file_folder}/result.css`;
+    return `${process.env.CDN_ROOT || ""}/api/font-source/${file_folder}/result.css`;
 };
