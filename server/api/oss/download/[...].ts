1cbcb962b2402febb4dcab88db356f4886b8d065
江夏尧 <3446798488@qq.com>
2025-02-11 23:28:55 +0800

    fix: 修复字体问题

--- server/api/oss/download/[...].ts
+++ server/api/oss/download/[...].ts
@@ -2,7 +2,7 @@ import { cacheLayer } from "~/server/utils/cache";
 import { useAfterResponse } from "~/server/utils/compose";
 import { getContentType } from "~/server/utils/contentType";
 import { s3Client } from "~/server/utils/s3Client";
-
+import type { Readable } from "node:stream";
 export default defineCompose(
     (event) => {
         useAfterResponse(event, () => {
@@ -16,7 +16,22 @@ export default defineCompose(
     cacheLayer(),
     async (event) => {
         const extra = event.path.split("/download/", 2)[1];
-        const res = await s3Client.getObject("chinese-fonts", `origin/${decodeURIComponent(extra)}`);
-        return res;
+        const res = await s3Client.getObject("chinese-fonts", `origin/${decodeURIComponent(extra)}`, {});
+        return readableToBlob(res);
     }
 );
+async function readableToBlob(readable: Readable) {
+    // 收集所有的数据片断
+    const chunks = [];
+    for await (const chunk of readable) {
+        chunks.push(chunk);
+    }
+
+    // 将所有数据片断合并成单一的Buffer
+    const combinedBuffer = Buffer.concat(chunks);
+
+    // 创建Blob对象，第二个参数是MIME类型，根据你的需求调整
+    const blob = new Blob([combinedBuffer], { type: "application/octet-stream" });
+
+    return blob;
+}
